72ca0e4993b1e23f1492cfa23f2ad03e290e1eb9
SukmawanID <sukmawan0425@gmail.com>
2024-01-11 12:32:30 +0700

    change name

--- appium/orderTicketfromapps.java
+++ appium/orderTicketfromapps.java
@@ -19,7 +19,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class orderTicketfromtraveloka {
+public class orderTicketfromapps {
 	@Test
 	public void orderTicket() throws MalformedURLException, InterruptedException{
 		
