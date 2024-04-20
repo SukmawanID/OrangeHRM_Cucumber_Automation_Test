ab9feec0af3fe2759a021079354c4f14eaa95400
SukmawanID <50280640+SukmawanID@users.noreply.github.com>
2024-04-20 20:06:24 +0700

    Delete selenium/Tokopedia directory

--- selenium/Tokopedia/src/main/java/utilities/url.java
+++
@@ -1,8 +0,0 @@
-package utilities;
-
-import org.openqa.selenium.WebDriver;
-
-public class url {
-    public static String tokopedia_homepage_url = "https://www.tokopedia.com/";
-    public static String orange_hrm_url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
-}
